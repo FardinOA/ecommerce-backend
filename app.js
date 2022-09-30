a98c3a008ac1a02ded2cd13a5b0210ef61db1691
Fardin Omor Afnan <afnan.eu.cse@gmail.com>
2022-09-30 22:53:38 +0600

    cros added

--- app.js
+++ app.js
@@ -14,7 +14,12 @@ require("dotenv").config();
 
 app.use(
     cors({
-        origin: ["http://localhost:3000"],
+        origin: [
+            "http://localhost:3000",
+            "https://ecommerce-frontend-fardinoa.vercel.app",
+            "https://ecommerce-frontend-git-main-fardinoa.vercel.app",
+            "https://ecommerce-frontend-theta.vercel.app",
+        ],
         credentials: true,
     })
 );
